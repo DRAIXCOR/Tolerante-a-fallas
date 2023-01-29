c4cb8f238f964114566a7f9eca70d77fc9bf6b58
DRAIXCOR <123282785+DRAIXCOR@users.noreply.github.com>
2023-01-28 23:37:58 -0600

    Add files via upload

---
+++ torelante a fallas codigos/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int c;
+    cout << "Hola mundo" << endl;
+    
+
+    cout << "Ingrese Entero"<< endl;
+    cin >> c ;
+  
+    cout << c<< endl; 
+    cout << "No es flotante pero aun así se ejecuta" << endl;
+    return 0;
+}
